fb994a21bdaa98814f1af93021647ff6bd9cc4c5
Eric Marquez <kenkm963@gmail.com>
2018-10-24 11:42:45 -0700

    testing out hello world

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ var express = require('express');
 var app = express();
 var server = app.listen(3000);
 
-app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
+app.listen(3000, () => console.log('Listening on port 3000'));
 
-
-console.log("Server is running");
+// app.use(express.static('public'));
 
